91f258e3dee8cbe4622426be7373eaa6a9cdec1a
hellloy <hellloy@gmail.com>
2024-07-23 15:01:07 +0300

    Initial commit

---
+++ src/api/framework/Infrastructure/Identity/Users/Endpoints/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Routing;
+
+namespace FSH.Framework.Infrastructure.Identity.Users.Endpoints;
+internal static class Extensions
+{
+    public static IEndpointRouteBuilder MapUserEndpoints(this IEndpointRouteBuilder app)
+    {
+        app.MapRegisterUserEndpoint();
+        app.MapUpdateUserEndpoint();
+        app.MapGetUsersListEndpoint();
+        app.MapDeleteUserEndpoint();
+        app.MapForgotPasswordEndpoint();
+        app.MapChangePasswordEndpoint();
+        app.MapResetPasswordEndpoint();
+        app.MapGetMeEndpoint();
+        app.MapGetUserEndpoint();
+        app.MapGetCurrentUserPermissionsEndpoint();
+        return app;
+    }
+}
---
+++ src/api/framework/Infrastructure/Identity/Users/Services/UserService.cs
@@ -0,0 +1,210 @@
+﻿using System.Collections.ObjectModel;
+using System.Security.Claims;
+using System.Text;
+using Finbuckle.MultiTenant.Abstractions;
+using FSH.Framework.Core.Caching;
+using FSH.Framework.Core.Exceptions;
+using FSH.Framework.Core.Identity.Users.Abstractions;
+using FSH.Framework.Core.Identity.Users.Dtos;
+using FSH.Framework.Core.Identity.Users.Features.RegisterUser;
+using FSH.Framework.Core.Identity.Users.Features.ToggleUserStatus;
+using FSH.Framework.Core.Identity.Users.Features.UpdateUser;
+using FSH.Framework.Core.Jobs;
+using FSH.Framework.Core.Mail;
+using FSH.Framework.Core.Tenant;
+using FSH.Framework.Infrastructure.Constants;
+using FSH.Framework.Infrastructure.Identity.Persistence;
+using FSH.Framework.Infrastructure.Identity.Roles;
+using FSH.Framework.Infrastructure.Tenant;
+using Mapster;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FSH.Framework.Infrastructure.Identity.Users.Services;
+
+internal sealed partial class UserService(
+    UserManager<FshUser> userManager,
+    SignInManager<FshUser> signInManager,
+    RoleManager<FshRole> roleManager,
+    IdentityDbContext db,
+    ICacheService cache,
+    IJobService jobService,
+    IMailService mailService,
+    IMultiTenantContextAccessor<FshTenantInfo> multiTenantContextAccessor
+    ) : IUserService
+{
+    private void EnsureValidTenant()
+    {
+        if (string.IsNullOrWhiteSpace(multiTenantContextAccessor?.MultiTenantContext?.TenantInfo?.Id))
+        {
+            throw new UnauthorizedException("invalid tenant");
+        }
+    }
+
+    public Task<string> ConfirmEmailAsync(string userId, string code, string tenant, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<string> ConfirmPhoneNumberAsync(string userId, string code)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> ExistsWithEmailAsync(string email, string? exceptId = null)
+    {
+        EnsureValidTenant();
+        return await userManager.FindByEmailAsync(email.Normalize()) is FshUser user && user.Id != exceptId;
+    }
+
+    public async Task<bool> ExistsWithNameAsync(string name)
+    {
+        EnsureValidTenant();
+        return await userManager.FindByNameAsync(name) is not null;
+    }
+
+    public async Task<bool> ExistsWithPhoneNumberAsync(string phoneNumber, string? exceptId = null)
+    {
+        EnsureValidTenant();
+        return await userManager.Users.FirstOrDefaultAsync(x => x.PhoneNumber == phoneNumber) is FshUser user && user.Id != exceptId;
+    }
+
+    public async Task<UserDetail> GetAsync(string userId, CancellationToken cancellationToken)
+    {
+        var user = await userManager.Users
+            .AsNoTracking()
+            .Where(u => u.Id == userId)
+            .FirstOrDefaultAsync(cancellationToken);
+
+        _ = user ?? throw new NotFoundException("user not found");
+
+        return user.Adapt<UserDetail>();
+    }
+
+    public Task<int> GetCountAsync(CancellationToken cancellationToken) =>
+        userManager.Users.AsNoTracking().CountAsync(cancellationToken);
+
+    public async Task<List<UserDetail>> GetListAsync(CancellationToken cancellationToken)
+    {
+        var users = await userManager.Users.AsNoTracking().ToListAsync(cancellationToken);
+        return users.Adapt<List<UserDetail>>();
+    }
+
+    public Task<string> GetOrCreateFromPrincipalAsync(ClaimsPrincipal principal)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<RegisterUserResponse> RegisterAsync(RegisterUserCommand request, string origin, CancellationToken cancellationToken)
+    {
+        // create user entity
+        var user = new FshUser
+        {
+            Email = request.Email,
+            FirstName = request.FirstName,
+            LastName = request.LastName,
+            UserName = request.UserName,
+            PhoneNumber = request.PhoneNumber,
+            IsActive = true,
+            EmailConfirmed = true
+        };
+
+        // register user
+        var result = await userManager.CreateAsync(user, request.Password);
+        if (!result.Succeeded)
+        {
+            var errors = result.Errors.Select(error => error.Description).ToList();
+            throw new FshException("error while registering a new user", errors);
+        }
+
+        // add basic role
+        await userManager.AddToRoleAsync(user, IdentityConstants.Roles.Basic);
+
+        // send confirmation mail
+        if (!string.IsNullOrEmpty(user.Email))
+        {
+            string emailVerificationUri = await GetEmailVerificationUriAsync(user, origin);
+            var mailRequest = new MailRequest(
+                new Collection<string> { user.Email },
+                "Confirm Registration",
+                emailVerificationUri);
+            jobService.Enqueue("email", () => mailService.SendAsync(mailRequest, CancellationToken.None));
+        }
+
+        return new RegisterUserResponse(user.Id);
+    }
+
+    public async Task ToggleStatusAsync(ToggleUserStatusCommand request, CancellationToken cancellationToken)
+    {
+        var user = await userManager.Users.Where(u => u.Id == request.UserId).FirstOrDefaultAsync(cancellationToken);
+
+        _ = user ?? throw new NotFoundException("User Not Found.");
+
+        bool isAdmin = await userManager.IsInRoleAsync(user, IdentityConstants.Roles.Admin);
+        if (isAdmin)
+        {
+            throw new FshException("Administrators Profile's Status cannot be toggled");
+        }
+
+        user.IsActive = request.ActivateUser;
+
+        await userManager.UpdateAsync(user);
+    }
+
+    public async Task UpdateAsync(UpdateUserCommand request, string userId)
+    {
+        var user = await userManager.FindByIdAsync(userId);
+
+        _ = user ?? throw new NotFoundException("User Not Found.");
+
+        user.FirstName = request.FirstName;
+        user.LastName = request.LastName;
+        user.PhoneNumber = request.PhoneNumber;
+        string? phoneNumber = await userManager.GetPhoneNumberAsync(user);
+        if (request.PhoneNumber != phoneNumber)
+        {
+            await userManager.SetPhoneNumberAsync(user, request.PhoneNumber);
+        }
+
+        var result = await userManager.UpdateAsync(user);
+        await signInManager.RefreshSignInAsync(user);
+
+        if (!result.Succeeded)
+        {
+            throw new FshException("Update profile failed");
+        }
+    }
+
+    public async Task DeleteAsync(string userId)
+    {
+        FshUser? user = await userManager.FindByIdAsync(userId);
+
+        _ = user ?? throw new NotFoundException("User Not Found.");
+
+        user.IsActive = false;
+        IdentityResult? result = await userManager.UpdateAsync(user);
+
+        if (!result.Succeeded)
+        {
+            List<string> errors = result.Errors.Select(error => error.Description).ToList();
+            throw new FshException("Delete profile failed", errors);
+        }
+    }
+
+    private async Task<string> GetEmailVerificationUriAsync(FshUser user, string origin)
+    {
+        EnsureValidTenant();
+
+        string code = await userManager.GenerateEmailConfirmationTokenAsync(user);
+        code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+        const string route = "api/users/confirm-email/";
+        var endpointUri = new Uri(string.Concat($"{origin}/", route));
+        string verificationUri = QueryHelpers.AddQueryString(endpointUri.ToString(), QueryStringKeys.UserId, user.Id);
+        verificationUri = QueryHelpers.AddQueryString(verificationUri, QueryStringKeys.Code, code);
+        verificationUri = QueryHelpers.AddQueryString(verificationUri,
+            TenantConstants.Identifier,
+            multiTenantContextAccessor?.MultiTenantContext?.TenantInfo?.Id!);
+        return verificationUri;
+    }
+}
